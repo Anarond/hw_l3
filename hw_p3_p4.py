2078d1388b7a9818276af5fff56e4428709fd336
aleksandr kuznetsov <anarond@gmail.com>
2025-11-01 09:23:20 +0300

    add another def, rename

--- hw_p3_p4.py
+++ hw_p3_p4.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver():
     opts = Options()
-
     opts.add_argument("--headless=new")
     opts.add_argument("--window-size=1280,900")
     driver = webdriver.Chrome(options=opts)
@@ -15,14 +14,17 @@ def driver():
     driver.quit()
 
 
-def test_selenium_web(driver):
-    url1 = "https://www.google.com/"
-    url2 = "https://github.com/"
-    driver.get(url1)
+def test_google_web(driver):
+    url_google = "https://www.google.com/"
+    driver.get(url_google)
     assert driver.title == "Google"
-    assert driver.current_url == url1
-    driver.get(url2)
+    assert driver.current_url == url_google
+
+
+def test_github_web(driver):
+    url_github = "https://github.com/"
+    driver.get(url_github)
     assert (
-        driver.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
+            driver.title == "GitHub · Change is constant. GitHub keeps you ahead. · GitHub"
     )
-    assert driver.current_url == url2
+    assert driver.current_url == url_github
